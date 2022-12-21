306413f4ffebc9001ebfa070a5d7e8f6a81536ef
Kaleb Olson <kaleb@coreform.com>
2022-12-21 04:57:13 +0000

    Fix issues with checking doom-nvim branch

--- bin/executable_setup_doom_nvim.sh
+++ bin/executable_setup_doom_nvim.sh
@@ -1,2 +1,5 @@
-git clone https://github.com/kalebo/doom-nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
-git checkout kalebo
+#!/usr/bin/env bash
+_nvim_dot_config=${XDG_CONFIG_HOME:-$HOME/.config}/nvim
+
+git clone https://github.com/kalebo/doom-nvim.git $_nvim_dot_config
+GIT_DIR=$_nvim_dot_config/.git GIT_WORK_TREE=$_nvim_dot_config git checkout kalebo 
